19854fd071c1f6967b40f465fdddf0bf434a203b
shin19 <github@shin19.net>
2019-04-09 00:28:07 +0900

    ツイート詳細ダイアログにURLコピーリンクを追加

--- app/src/main/java/org/dragonegg/ofuton/fragment/dialog/StatusDialogFragment.java
+++ app/src/main/java/org/dragonegg/ofuton/fragment/dialog/StatusDialogFragment.java
@@ -24,6 +24,7 @@ import org.dragonegg.ofuton.action.ClickActionAdapter;
 import org.dragonegg.ofuton.action.status.CancelRetweetAction;
 import org.dragonegg.ofuton.action.status.ClickAction;
 import org.dragonegg.ofuton.action.status.ConversationAction;
+import org.dragonegg.ofuton.action.status.CopyLinkAction;
 import org.dragonegg.ofuton.action.status.DestroyStatusAction;
 import org.dragonegg.ofuton.action.status.FavAction;
 import org.dragonegg.ofuton.action.status.FavAndRetweeAction;
@@ -231,6 +232,11 @@ public class StatusDialogFragment extends DialogFragment {
 
         if(PrefUtil.getBoolean(R.string.show_hashtag, true)){
             setHashtagEntities(mSelectedStatus);
+        }
+
+        // URLコピー
+        if (PrefUtil.getBoolean(R.string.show_link_copy, true)) {
+            mActionAdapter.add(new CopyLinkAction(getContext(), mSelectedStatus));
         }
 
         mActionAdapter.notifyDataSetChanged();
